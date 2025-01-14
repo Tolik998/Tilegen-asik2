5d9e726d7211c5b6b8665a5e44a3503ed05e396d
Tolik998 <tolegentilegen3@gmail.com>
2025-01-15 02:20:28 +0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,33 +1,40 @@
 import java.util.Objects;
 
+// The Customer class represents a bank customer
 public class Customer {
-    private String name;
-    private String id;
-    private Account account;
+    private String name;    // Customer name
+    private String id;      // Unique customer ID
+    private Account account; // Customer's account
 
+    // Constructor to initialize a customer
     public Customer(String id, String name, Account account) {
         this.id = id;
         this.name = name;
         this.account = account;
     }
 
+    // Get the customer's name
     public String getName() {
         return name;
     }
 
+    // Get the customer's ID
     public String getId() {
         return id;
     }
 
+    // Get the customer's account
     public Account getAccount() {
         return account;
     }
 
+    // String representation of the customer
     @Override
     public String toString() {
         return "Customer{id='" + id + "', name='" + name + "', " + account + "}";
     }
 
+    // Compare customers by ID
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -36,8 +43,14 @@ public class Customer {
         return Objects.equals(id, customer.id);
     }
 
+    // Generate a hash code for the customer
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    // Print customer details
+    public void printDetails() {
+        System.out.println(this);
     }
 }
