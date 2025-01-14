80debade423c5a2559d9a3b4f8f087a3a746395a
Tolik998 <tolegentilegen3@gmail.com>
2025-01-15 02:20:58 +0500

    Create PremiumCustomer.java

---
+++ PremiumCustomer.java
@@ -0,0 +1,35 @@
+// The PremiumCustomer class extends the functionality of a basic customer
+public class PremiumCustomer extends Customer {
+    private double cashbackRate; // Cashback rate
+
+    // Constructor for a premium customer
+    public PremiumCustomer(String id, String name, Account account, double cashbackRate) {
+        super(id, name, account);
+        this.cashbackRate = cashbackRate;
+    }
+
+    // Get the cashback rate
+    public double getCashbackRate() {
+        return cashbackRate;
+    }
+
+    // Set a new cashback rate
+    public void setCashbackRate(double cashbackRate) {
+        this.cashbackRate = cashbackRate;
+    }
+
+    // Apply cashback to the customer's account
+    public void applyCashback() {
+        double cashback = getAccount().getBalance() * cashbackRate;
+        getAccount().deposit(cashback);
+    }
+
+    // String representation of a premium customer
+    @Override
+    public String toString() {
+        return "PremiumCustomer{" +
+                "cashbackRate=" + cashbackRate +
+                ", details=" + super.toString() +
+                '}';
+    }
+}
