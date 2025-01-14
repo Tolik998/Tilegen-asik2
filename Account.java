3a2e4c3af9845adfdf807c77b396247af84840c9
Tolik998 <tolegentilegen3@gmail.com>
2025-01-15 02:18:57 +0500

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,28 +1,34 @@
 import java.util.Objects;
 
+// The Account class represents a bank account
 public class Account {
-    private String accountNumber;
-    private double balance;
+    private String accountNumber; // Account number
+    private double balance;       // Account balance
 
+    // Constructor to initialize the account
     public Account(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
     }
 
+    // Get the account number
     public String getAccountNumber() {
         return accountNumber;
     }
 
+    // Get the current balance
     public double getBalance() {
         return balance;
     }
 
+    // Deposit money into the account
     public void deposit(double amount) {
         if (amount > 0) {
             balance += amount;
         }
     }
 
+    // Withdraw money from the account
     public boolean withdraw(double amount) {
         if (amount > 0 && amount <= balance) {
             balance -= amount;
@@ -31,11 +37,13 @@ public class Account {
         return false;
     }
 
+    // String representation of the account
     @Override
     public String toString() {
         return "Account{accountNumber='" + accountNumber + "', balance=$" + balance + "}";
     }
 
+    // Compare accounts by account number
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -44,6 +52,7 @@ public class Account {
         return Objects.equals(accountNumber, account.accountNumber);
     }
 
+    // Generate a hash code for the account
     @Override
     public int hashCode() {
         return Objects.hash(accountNumber);
